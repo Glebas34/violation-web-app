211cbaf0365f85938ba43273d21e652f3cf8d63a
Gleb Kuimov <kuimov.gleb30122002@gmail.com>
2023-11-04 22:19:59 +0300

    Добавление enum с типами нарушений

---
+++ ViolationWebApplication/Data/ViolationType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViolationWebApplication.Data
+{
+    public enum ViolationType
+    {
+        [Display(Name = "Превышение скорости")]
+        Speeding,
+        [Display(Name = "Пьяное вождение")]
+        DrunkDriving,
+        [Display(Name = "Пересечение двойной сплошной")]
+        IntersectionOfASolidMarkingLine
+    }
+}
