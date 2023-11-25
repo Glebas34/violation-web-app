2bbccb924334b68d068add9ab6218f21e550d748
Gleb Kuimov <kuimov.gleb30122002@gmail.com>
2023-11-25 20:35:10 +0300

    Добавил профиль

--- ViolationWebApplication/wwwroot/js/site.js
+++ ViolationWebApplication/wwwroot/js/site.js
@@ -2,21 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function initMasks() {
-    cardNumberMask = IMask($cardNumberField, {
-        mask: binking.defaultResult.cardNumberMask
-    })
-    monthMask = IMask($monthField, {
-        mask: IMask.MaskedRange,
-        from: 1,
-        to: 12,
-        maxLength: 2,
-        autofix: true
-    })
-    yearMask = IMask($yearField, {
-        mask: '00'
-    })
-    codeMask = IMask($codeField, {
-        mask: '0000'
-    })
-}
